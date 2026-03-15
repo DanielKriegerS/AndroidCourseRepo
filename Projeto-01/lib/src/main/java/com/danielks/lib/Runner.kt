d0372518763c9641e277e03f89f0eac94650c4ca
DanielKriegerS <danielschifelbain@gmail.com>
2026-03-15 15:55:03 -0300

    Project 01: created basic runner class

---
+++ Projeto-01/lib/src/main/java/com/danielks/lib/Runner.kt
@@ -0,0 +1,12 @@
+package com.danielks.lib
+
+class Runner {
+    init {
+        initialize()
+    }
+
+    private fun initialize() {
+        val processor = Processor()
+        var menu = Menu(processor)
+    }
+}
